6cefc5c125bc437d177b673a11ed025f329bd496
Bishal <bishalshah431@gmail.com>
2024-11-17 11:43:18 +0545

    test-1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "apple", "banana" , "orange", "mango", "pineapple" };
+
+            // LINQ query to select names that contain the letter 'k'
+            var a = from n in name
+                    where n.Length > 5
+                    select n;
+
+            // Iterate through the filtered names and print them
+            foreach (string item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
