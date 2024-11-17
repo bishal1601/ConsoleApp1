2b1838bad0157a7ed8a5c08a9970d5b5c9fa5610
Bishal <bishalshah431@gmail.com>
2024-11-17 12:13:16 +0545

    car search

--- Program.cs
+++ Program.cs
@@ -7,20 +7,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[] name = { "apple", "banana" , "orange", "mango", "pineapple" };
-
-            // LINQ query to select names that contain the letter 'k'
-            var a = from n in name
-                    where n.Length > 5
-                    select n;
-
-            // Iterate through the filtered names and print them
-            foreach (string item in a)
+            List<Car> myCars = new List<Car>()
             {
-                Console.WriteLine(item);
-            }
+                new Car() { Cname = "Car1", Make = "BMW", Model = "dd", Year = 1999 },
+                new Car() { Cname = "Car2", Make = "Audi", Model = "ioiuy", Year = 1888 },
+                new Car() { Cname = "car3", Make="Toyoto", Model = "5445", Year = 2000 },
+                new Car() { Cname = "car4", Make = "Ford", Model = "4545", Year = 1799 },
 
-            Console.ReadLine();
+            };
+            var cars = myCars.Where(x => x.Cname == "Car1").ToList();
+            foreach (var checkcar in cars)
+            {
+                Console.WriteLine( checkcar.Cname);
+            }
+            
         }
+        
     }
+
+    
+}
+class Car
+{
+    public string Cname{ get; set; }
+    public string Model{ get; set; }
+    public string Make{ get; set; }
+    public int Year{ get; set; }
 }
--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cefc5c125bc437d177b673a11ed025f329bd496")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
